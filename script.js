7e25213693eedc8a158e996b8393578c7ee57579
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-04 03:47:01 +0500

    add playRound func to play a single round of game

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 function getComputerChoice() {
-
     // This will either give 0, or 1, or 2
     const rand = Math.floor(Math.random()* (3));
-    
-    console.log(rand);
 
     if (rand == 0){
-        return "Rock";
+        return "ROCK";
     }
 
     else if (rand == 1) {
-        return "Paper";
+        return "PAPER";
     }
 
     else{
-        return "Scissors";
+        return "SCISSORS";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+
+    console.log(`Your Choice: ${playerSelection}\nComputer's Choice: ${computerSelection}`);
+
+    if (playerSelection == computerSelection) {
+        return "It's a Tie";
+    }
+
+    else if (
+         ((playerSelection == "ROCK") && (computerSelection == "PAPER")) || 
+         ((playerSelection == "PAPER") && (computerSelection == "SCISSORS")) || 
+         ((playerSelection == "SCISSORS") && (computerSelection == "ROCK"))
+         ) {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+         }
+    else {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
+
