9738fbb80726813e0bef75fbdc629d7a5d149e5b
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-18 22:34:50 +0500

    Update Logic 1.  remove getResult function as not necessary     anymore. 2.  update code logic better suited for UI.

--- script.js
+++ script.js
@@ -15,21 +15,6 @@ function getComputerChoice() {
     }
 }
 
-function getResult(playerCount, compCount) {
-    const matchResultText  = document.createElement("p");
-    if (playerCount > compCount) {     
-        matchResultText.textContent = "Congrats, You Won The Match!";
-    }
-    else if (playerCount < compCount) {
-        matchResultText.textContent = "You Lost, Better Luck Next Time!";
-    }
-    else{
-        matchResultText.textContent = "It's a Tie!";
-    }
-
-    return matchResultText;
-}
-
 function playRound(playerSelection, computerSelection) {
 
     console.log(`Your Choice: ${playerSelection}\nComputer's Choice: ${computerSelection}`);
@@ -53,29 +38,14 @@ function playRound(playerSelection, computerSelection) {
     return roundResultText;
 }
 
-function playGame() {
-    let playerCount = 0;
-    let compCount = 0;
-    let playerSelection;
+function updateGameStatus(playerSelection, playerCount, compCount) {
 
-    const rockButton = document.createElement("button");
-    const paperButton = document.createElement("button");
-    const scissorsButton = document.createElement("button");
+    const roundResultText = document.createElement("p");
+    const scoreText = document.createElement("p");
 
-    rockButton.addEventListener("click", playRound("ROCK", getComputerChoice()));
-    paperButton.addEventListener("click", playRound("PAPER", getComputerChoice()));
-    scissorsButton.addEventListener("click", playRound("SCISSORS", getComputerChoice()));
+    roundResultText = playRound(playerSelection, getComputerChoice());
 
-    const resultBox = document.createElement("div");
-
-    const body = document.querySelector("body");
-    body.appendChild(rockButton);
-    body.appendChild(paperButton);
-    body.appendChild(scissorsButton);
-    body.appendChild(resultBox);
-
-    const resultString = playRound(playerSelection, getComputerChoice());
-    const checkString = resultString.substring(4,7);
+    const checkString = roundResultText.getTextContent().substring(4,7);
     if (checkString == "Win") {
         playerCount++;
     }
@@ -83,9 +53,86 @@ function playGame() {
         compCount++;
     }
     
-    console.log(resultString);
-    console.log(`Score:\nPlayer: ${playerCount}\nComputer: ${compCount}`);
+    scoreText = `Player: ${playerCount} Computer: ${compCount}`;
+
+    return [roundResultText, scoreText, playerCount, compCount];
 }
 
-  
+function checkMatchScore(playerCount, compCount) {
+    if (playerCount == 5) {
+        return 1; //    Win
+    }
+
+    else if (compCount == 5) {
+        return -1; //   Lose
+    }
+
+    else {
+        return 0;   //  Match hasn't ended yet
+    }
+}
+
+function resetGame(result) {
+    const resetBoard = document.createElement("div");
+    const finalResult = document.createElement("p");
+    const resetButton  = document.createElement("button");
+    resetButton.textContent = "Reset";
+
+    if (result === 1) {
+        finalResult.textContent = "You Won The Match!";
+    }
+
+    else {
+        finalResult.textContent = "You Lost The Match";
+    }
+
+    resetBoard.appendChild(finalResult);
+    resetBoard.appendChild(resetButton);
+    const body = document.querySelector("body");
+    body.appendChild(resetBoard);
+}
+
+function playGame() {
+    const rockButton = document.createElement("button");
+    const paperButton = document.createElement("button");
+    const scissorsButton = document.createElement("button");
+
+    const resultBox = document.createElement("div");
+    let roundResultText;
+    let scoreText;
+
+    let playerCount = 0;
+    let compCount = 0;
+
+    rockButton.addEventListener("click", () => {
+        [roundResultText, scoreText, playerCount, compCount] = updateGameStatus("ROCK", playerCount, compCount);
+        let gameCheck = checkMatchScore(playerCount, compCount);
+        if (gameCheck !== 0) {
+            resetGame(gameCheck);
+        }
+    });
+    paperButton.addEventListener("click", () => {
+        [roundResultText, scoreText, playerCount, compCount] = updateGameStatus("PAPER", playerCount, compCount);
+        checkMatchScore(playerCount, compCount);
+        if (gameCheck !== 0) {
+            resetGame(gameCheck);
+        }
+    });
+    scissorsButton.addEventListener("click", () => {
+        [roundResultText, scoreText, playerCount, compCount] = updateGameStatus("SCISSORS", playerCount, compCount);
+        checkMatchScore(playerCount, compCount);
+        if (gameCheck !== 0) {
+            resetGame(gameCheck);
+        }
+    });
+
+    resultBox.appendChild(resultText);
+    resultBox.appendChild(scoreText);
+    const body = document.querySelector("body");
+    body.appendChild(rockButton);
+    body.appendChild(paperButton);
+    body.appendChild(scissorsButton);
+    body.appendChild(resultBox);
+}
+
 playGame();
